0c6c14f9524fabfe38d236f6742e0f4475918abb
luiz-kohler <luizfelipekohler03@gmail.com>
2025-08-28 18:20:36 -0300

    feat: creating pets endpoints structure

---
+++ API/Controllers/PetController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("pets")]
+    public class PetController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create([FromBody] PetRequest request)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Get([FromRoute] int id, [FromBody] PetRequest request)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            return Ok();
+        }
+    }
+}
