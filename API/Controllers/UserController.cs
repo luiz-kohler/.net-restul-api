57d449805d972afcbb26dcbe007dd0955e3981f2
luiz-kohler <luizfelipekohler03@gmail.com>
2025-08-28 18:18:18 -0300

    feat: creating users endpoints structures

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateUserRequest request)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Get([FromRoute] int id, [FromBody] UpdateUserRequest request)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] CreateUserRequest request)
+        {
+            return Ok();
+        }
+    }
+}
