8e1d1f6274dbd660d9a289fcfb2e31641dd556fc
luiz-kohler <luizfelipekohler03@gmail.com>
2025-08-28 07:47:46 -0300

    feat: creating entities

---
+++ API/Entities.cs
@@ -0,0 +1,23 @@
+﻿namespace API
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string HashedPassword { get; set; }
+        public required string ETag { get; set; }
+        public DateTimeOffset? LastModifiedAt { get; set; }
+    }
+
+    public class Pet
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public required string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsVaccinated { get; set; }
+        public required string ETag { get; set; }
+        public DateTimeOffset? LastModifiedAt { get; set; }
+    }
+}
