5f6def600bd42fee8207f85a1bdc166eb7264cd6
luiz-kohler <luizfelipekohler03@gmail.com>
2025-08-28 07:47:55 -0300

    feat:  creating view models

---
+++ API/ViewModels.cs
@@ -0,0 +1,19 @@
+﻿namespace API
+{
+    #region User
+
+    public record UserRequest(string Name, string Password);
+    public record BasicUserResponse(int Id, string Name);
+    public record DetailedUserResponse(int Id, int UserId, string UserName, string Name, int Age, bool IsVaccinated, IEnumerable<BasicPetResponse> Pets);
+
+    #endregion
+
+    #region Pet
+
+    public record PetRequest(int UserId, string Name, int Age, bool IsVaccinated);
+    public record BasicPetResponse(int Id, int UserId, string Name, int Age, bool IsVaccinated);
+    public record DetailedPetResponse(int Id, int UserId, string UserName, string Name, int Age, bool IsVaccinated);
+
+    #endregion
+
+}
