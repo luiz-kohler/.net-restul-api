1f11843e0bc572868660f54bd27489b6dd59b2f4
luiz-kohler <luizfelipekohler03@gmail.com>
2025-08-29 08:07:55 -0300

    feat: base entity and implementing

--- API/Entities.cs
+++ API/Entities.cs
@@ -1,23 +1,24 @@
 ﻿namespace API
 {
-    public class User
+    public abstract class BaseEntity
     {
         public int Id { get; set; }
+        public DateTimeOffset? LastModifiedAt { get; set; }
+        public required string ETag { get; set; }
+    }
+
+    public class User : BaseEntity
+    {
         public required string Name { get; set; }
         public required string Email { get; set; }
         public required string HashedPassword { get; set; }
-        public required string ETag { get; set; }
-        public DateTimeOffset? LastModifiedAt { get; set; }
     }
 
-    public class Pet
+    public class Pet : BaseEntity
     {
-        public int Id { get; set; }
         public int UserId { get; set; }
         public required string Name { get; set; }
         public int Age { get; set; }
         public bool IsVaccinated { get; set; }
-        public required string ETag { get; set; }
-        public DateTimeOffset? LastModifiedAt { get; set; }
     }
 }
