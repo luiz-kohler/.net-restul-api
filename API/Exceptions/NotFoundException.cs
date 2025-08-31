8b5f9e3ea4df0057b98121c38c12f707e0320eda
luiz-kohler <luizfelipekohler03@gmail.com>
2025-08-31 16:14:48 -0300

    feat: adding some new exceptions

---
+++ API/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Exceptions
+{
+    [Serializable]
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() : base() { }
+        public NotFoundException(string message) : base(message) { }
+        public NotFoundException(string message, Exception inner) : base(message, inner) { }
+    }
+}
