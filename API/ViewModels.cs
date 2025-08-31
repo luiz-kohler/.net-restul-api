b32c1b9935aa6dc8c340f3971a5ecfabc7774149
luiz-kohler <luizfelipekohler03@gmail.com>
2025-08-31 16:16:13 -0300

    fix: view models w/ default constructors

--- API/ViewModels.cs
+++ API/ViewModels.cs
@@ -1,43 +1,86 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Xml.Linq;
 
 namespace API
 {
     #region User
 
-    public record CreateUserRequest(
-        [property: Required] [property: MinLength(2)] string Name,
-        [property: Required] [property: EmailAddress] string Email, 
-        [property: Required] [property: MinLength(4)] string Password
-    );
+    public class CreateUserRequest
+    {
+        [Required]
+        [MinLength(2)]
+        public required string Name { get; set; }
 
-    public record UpdateUserRequest(
-        [property: Required] [property: MinLength(2)] string Name
-    );
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
 
-    public record LoginRequest(
-        [property: Required] [property: EmailAddress] string Email,
-        [property: Required] [property: MinLength(4)] string Password
-    );
+        [Required]
+        [MinLength(4)]
+        public required string Password { get; set; }
+    }
 
-    public record BasicUserResponse(int Id, string Name);
-    public record DetailedUserResponse(int Id, string Name, IEnumerable<BasicPetResponse> Pets) 
-                : BasicUserResponse(Id, Name);
+    public class UpdateUserRequest
+    {
+        [Required]
+        [MinLength(2)]
+        public required string Name { get; set; }
+    }
+
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        public required string Password { get; set; }
+    }
+
+    public class BasicUserResponse
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+    }
+
+    public class DetailedUserResponse : BasicUserResponse
+    {
+        public required IEnumerable<BasicPetResponse> Pets { get; set; }
+    }
 
     #endregion
 
     #region Pet
 
-    public record PetRequest(
-        [property: Range(1, int.MaxValue)] int UserId,
-        [property: Required] [property: MinLength(2)] string Name,
-        [property: Range(1, 999)] int Age, 
-        bool IsVaccinated
-    );
+    public class PetRequest
+    {
+        [Range(1, int.MaxValue)]
+        public int UserId { get; set; }
 
-    public record BasicPetResponse(int Id, int UserId, string Name, int Age, bool IsVaccinated);
-    public record DetailedPetResponse(int Id, int UserId, string UserName, string Name, int Age, bool IsVaccinated)
-                : BasicPetResponse(Id, UserId, Name, Age, IsVaccinated);
+        [Required]
+        [MinLength(2)]
+        public required string Name { get; set; }
+
+        [Range(1, 999)]
+        public int Age { get; set; }
+
+        public bool IsVaccinated { get; set; }
+    }
+
+    public class BasicPetResponse
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public required string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsVaccinated { get; set; }
+    }
+
+    public class DetailedPetResponse : BasicPetResponse
+    {
+        public required string UserName { get; set; }
+    }
 
     #endregion
 }
